b9505bc5415773e9533cc0849d0e92b8dc5835de
Harry EMARH <emarh.harry.code@gmail.com>
2024-05-28 04:34:37 +0200

    A Working API Example: findAll, findOne and create

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -2,25 +2,38 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
 import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
+  //   [
+  //     { id: 1, name: 'First event' },
+  //     { id: 2, name: 'Second event' },
+  //   ]
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First event' },
-      { id: 2, name: 'Second event' },
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
-    return { id: 1, name: 'First event' };
+    // return { id: 1, name: 'First event' };
+    const event = this.events.find((event) => event.id === parseInt(id));
+
+    return event;
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+
+    return event;
   }
 
   @Patch(':id')
